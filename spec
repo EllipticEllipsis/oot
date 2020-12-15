6e6d84407b6257153026223caf537cf8b16ff382
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2020-12-15 03:08:40 +0000

    OK, data migrated

--- spec
+++ spec
@@ -2576,8 +2576,9 @@ endseg
 beginseg
     name "ovl_En_Jj"
     include "build/src/overlays/actors/ovl_En_Jj/z_en_jj.o"
-    include "build/data/overlays/actors/z_en_jj.data.o"
-    include "build/data/overlays/actors/z_en_jj.reloc.o"
+    include "build/src/overlays/actors/ovl_En_Jj/ovl_En_Jj_reloc.o"
+    //include "build/data/overlays/actors/z_en_jj.data.o"
+    //include "build/data/overlays/actors/z_en_jj.reloc.o"
 endseg
 
 beginseg
--- src/overlays/actors/ovl_En_Jj/z_en_jj.c
+++ src/overlays/actors/ovl_En_Jj/z_en_jj.c
@@ -15,13 +15,14 @@ void EnJj_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnJj_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnJj_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000A1C;
-extern UNK_TYPE D_06001830;
-extern UNK_TYPE D_06001F4C;
-extern UNK_TYPE D_0600B9A8;
-extern UNK_TYPE D_0600BA8C;
+void func_80A87BEC(EnJj* this, GlobalContext* globalCtx);
+void func_80A87C30(EnJj* this, GlobalContext* globalCtx);
+void func_80A87CEC(EnJj* this, GlobalContext* globalCtx);
+void func_80A87D94(EnJj* this, GlobalContext* globalCtx);
+void func_80A87EF0(EnJj* this, GlobalContext* globalCtx);
+void func_80A87F44(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+
 const ActorInit En_Jj_InitVars = {
     ACTOR_EN_JJ,
     ACTORTYPE_ITEMACTION,
@@ -33,29 +34,290 @@ const ActorInit En_Jj_InitVars = {
     (ActorFunc)EnJj_Update,
     (ActorFunc)EnJj_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87800.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Init.s")
+s32 usused = 0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Destroy.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87B1C.s")
+// extern void* D_80A88164;
+#include "z_en_jj_cutscene_data.c" EARLY
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87B9C.s")
+s32 usused2[] = { 0, 0 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87BEC.s")
+// sCylinderInit
+static ColliderCylinderInit D_80A88CB4 =
+{
+    { COLTYPE_UNK10, 0x00, 0x09, 0x39, 0x10, COLSHAPE_CYLINDER },
+    { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000004, 0x00, 0x00 }, 0x00, 0x01, 0x01 },
+    { 170, 150, 0, { 0, 0, 0 } },
+};
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87C30.s")
+// sInitChain
+static InitChainEntry D_80A88CE0[] = {
+    ICHAIN_VEC3F_DIV1000(scale, 87, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneForward, 4000, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneScale, 3300, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneDownward, 1100, ICHAIN_STOP),
+};
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87CEC.s")
+static Vec3f D_80A88CF0 = { -1589, 53, -43 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87D94.s")
+static Gfx* D_80A88CFC[] = { 0x06007698, 0x06007A98, 0x06007E98 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87EF0.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87F44.s")
+extern UNK_TYPE D_06000A1C;
+extern UNK_TYPE D_06001830;
+extern AnimationHeader D_06001F4C;
+extern FlexSkeletonHeader D_0600B9A8;
+extern UNK_TYPE D_0600BA8C;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Update.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87800.s")
+void func_80A87800(EnJj* this, EnJjActionFunc actionFunc) {
+    this->actionFunc = actionFunc;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Draw.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Init.s")
+void EnJj_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnJj* this = THIS;
+    s32 pad;
+    ColHeader* colHeader = NULL;
+
+    Actor_ProcessInitChain(&this->dyna.actor, D_80A88CE0);
+    ActorShape_Init(&this->dyna.actor.shape, 0.0f, NULL, 0.0f);
+
+    switch (this->dyna.actor.params) {
+        case -1:
+            SkelAnime_InitFlex(globalCtx, &this->skelanime, &D_0600B9A8, &D_06001F4C, this->limbDrawTable,
+                               this->transitionDrawTable, 0x16);
+            SkelAnime_ChangeAnimDefaultRepeat(&this->skelanime, &D_06001F4C);
+
+            this->unk_30A = 0;
+            this->unk_30E = 0;
+            this->unk_30F = 0;
+            this->unk_310 = 0;
+            this->unk_311 = 0;
+
+            if (gSaveContext.eventChkInf[3] & 0x400) {
+                func_80A87800(this, func_80A87BEC);
+            } else {
+                func_80A87800(this, func_80A87C30);
+            }
+
+            this->unk_300 = (DynaPolyActor*)Actor_SpawnAsChild(
+                &globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_EN_JJ, this->dyna.actor.posRot.pos.x - 10.0f,
+                this->dyna.actor.posRot.pos.y, this->dyna.actor.posRot.pos.z, 0, this->dyna.actor.posRot.rot.y, 0, 0);
+            DynaPolyInfo_SetActorMove(&this->dyna, 0);
+            DynaPolyInfo_Alloc(&D_06000A1C, &colHeader);
+            this->dyna.dynaPolyId =
+                DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
+            Collider_InitCylinder(globalCtx, &this->collider);
+            Collider_SetCylinder(globalCtx, &this->collider, &this->dyna.actor, &D_80A88CB4);
+            this->dyna.actor.colChkInfo.mass = 0xFF;
+            break;
+
+        case 0:
+            DynaPolyInfo_SetActorMove(&this->dyna, 0);
+            DynaPolyInfo_Alloc(&D_06001830, &colHeader);
+            this->dyna.dynaPolyId =
+                DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
+            func_8003ECA8(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            this->dyna.actor.update = func_80A87F44;
+            this->dyna.actor.draw = NULL;
+            Actor_SetScale(&this->dyna.actor, 0.087f);
+            break;
+
+        case 1:
+            DynaPolyInfo_SetActorMove(&this->dyna, 0);
+            DynaPolyInfo_Alloc(&D_0600BA8C, &colHeader);
+            this->dyna.dynaPolyId =
+                DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, colHeader);
+            this->dyna.actor.update = func_80A87F44;
+            this->dyna.actor.draw = NULL;
+            Actor_SetScale(&this->dyna.actor, 0.087f);
+            break;
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Destroy.s")
+void EnJj_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    EnJj* this = THIS;
+
+    switch (this->dyna.actor.params) {
+        case -1:
+            DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            Collider_DestroyCylinder(globalCtx, &this->collider);
+            break;
+
+        case 0:
+        case 1:
+            DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+            break;
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87B1C.s")
+void func_80A87B1C(EnJj* this) {
+    // u8 temp_t8;
+
+    if (this->unk_30F > 0) {
+        this->unk_30F--;
+    } else {
+        this->unk_30E++;
+        if (this->unk_30E >= 3) {
+            this->unk_30E = 0;
+            if (this->unk_310 > 0) {
+                this->unk_310--;
+            } else {
+                this->unk_30F = Math_Rand_S16Offset(20, 20);
+                this->unk_310 = this->unk_311;
+            }
+        }
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87B9C.s")
+void func_80A87B9C(EnJj* this, GlobalContext* globalCtx) {
+    DynaPolyActor* otherJabu = this->unk_300;
+
+    if (this->unk_308 >= -0x1450) {
+        this->unk_308 = (s16)(this->unk_308 - 0x66);
+        if (this->unk_308 < -0xA28) {
+            func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, otherJabu->dynaPolyId);
+        }
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87BEC.s")
+void func_80A87BEC(EnJj* this, GlobalContext* globalCtx) {
+    if (this->dyna.actor.xzDistFromLink < 300.0f) {
+        func_80A87800(this, func_80A87B9C);
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87C30.s")
+void func_80A87C30(EnJj* this, GlobalContext* globalCtx) {
+    Player* player = PLAYER;
+
+    if ((Math_Vec3f_DistXZ(&D_80A88CF0, &player->actor.posRot.pos) < 300.0f) &&
+        (globalCtx->isPlayerDroppingFish(globalCtx))) {
+        this->unk_30C = 100;
+        func_80A87800(this, func_80A87CEC);
+    }
+    this->collider.dim.pos.x = -1245;
+    this->collider.dim.pos.y = 20;
+    this->collider.dim.pos.z = -48;
+    CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87CEC.s")
+void func_80A87CEC(EnJj* this, GlobalContext* globalCtx) {
+    DynaPolyActor* otherJabu = this->unk_300;
+
+    if ((s32)this->unk_30C > 0) {
+        this->unk_30C--;
+    } else {
+        func_80A87800(this, func_80A87EF0);
+        globalCtx->csCtx.segment = &D_80A88164;
+        gSaveContext.cutsceneTrigger = 1;
+        func_8003EBF8(globalCtx, &globalCtx->colCtx.dyna, otherJabu->dynaPolyId);
+        func_8005B1A4(ACTIVE_CAM);
+        gSaveContext.eventChkInf[3] |= 0x400;
+        func_80078884(NA_SE_SY_CORRECT_CHIME);
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87D94.s")
+void func_80A87D94(EnJj* this, GlobalContext* globalCtx) {
+
+    switch (globalCtx->csCtx.npcActions[2]->action) {
+        case 1:
+            if ((this->unk_30A & 2) != 0) {
+                this->unk_30E = 0;
+                this->unk_30F = Math_Rand_S16Offset(20, 20);
+                this->unk_310 = 0;
+                this->unk_311 = 0;
+                this->unk_30A ^= 2;
+                // phi_v1 = this->unk_30A;
+            }
+            break;
+
+        case 2:
+            this->unk_30A |= 1;
+            if ((this->unk_30A & 8) == 0) {
+                this->unk_304 = Actor_SpawnAsChild(&globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_EFF_DUST,
+                                                   -1100.0f, 105.0f, -27.0f, 0, 0, 0, 0);
+                this->unk_30A |= 8;
+            }
+            break;
+
+        case 3:
+            if ((this->unk_30A & 2) == 0) {
+                this->unk_30E = 0;
+                this->unk_30F = 0;
+                this->unk_310 = 1;
+                this->unk_311 = 0;
+                this->unk_30A |= 2;
+            }
+            break;
+    }
+    if ((this->unk_30A & 1) != 0) {
+        Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_JABJAB_BREATHE - SFX_FLAG);
+        if (this->unk_308 >= -0x1450) {
+            this->unk_308 -= 0x66;
+        }
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87EF0.s")
+void func_80A87EF0(EnJj* this, GlobalContext* globalCtx) {
+    Actor* temp_a0;
+
+    if ((this->unk_30A & 4) == 0) {
+        this->unk_30A |= 4;
+        temp_a0 = this->unk_304;
+        if (temp_a0 != 0) {
+            this = this;
+            Actor_Kill(temp_a0);
+            this->dyna.actor.child = NULL;
+        }
+    }
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/func_80A87F44.s")
+void func_80A87F44(Actor* thisx, GlobalContext* globalCtx) {
+
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Update.s")
+void EnJj_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnJj* this = THIS;
+
+    if ((globalCtx->csCtx.state != 0) && (globalCtx->csCtx.npcActions[2] != 0)) {
+        func_80A87D94(this, globalCtx);
+    } else {
+        this->actionFunc(this, globalCtx);
+        if (this->skelanime.animCurrentFrame == 41.0f) {
+            Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_JABJAB_GROAN);
+        }
+    }
+    func_80A87B1C(this);
+    SkelAnime_FrameUpdateMatrix(&this->skelanime);
+    Actor_SetScale(&this->dyna.actor, 0.087f);
+    this->skelanime.limbDrawTbl[10].z = (s16)this->unk_308;
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Jj/EnJj_Draw.s")
+void EnJj_Draw(Actor *thisx, GlobalContext *globalCtx) {
+    EnJj *this = THIS;
+    s32 pad;
+
+    OPEN_DISPS(globalCtx->state.gfxCtx, "../z_en_jj.c", 879);
+    func_800943C8(globalCtx->state.gfxCtx);
+    Matrix_Translate(0.0f, (cosf(this->skelanime.animCurrentFrame * (M_PI/41.0f)) * 10.0f) - 10.0f, 0.0f, 1);
+    Matrix_Scale(10.0f, 10.0f, 10.0f, 1);
+    gSPSegment(POLY_OPA_DISP++, 0x08, SEGMENTED_TO_VIRTUAL(D_80A88CFC[this->unk_30E]));
+    SkelAnime_DrawFlexOpa(globalCtx, this->skelanime.skeleton, this->skelanime.limbDrawTbl,
+     this->skelanime.dListCount, 0, 0, this);
+    CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_en_jj.c", 898);
+}
+
+
